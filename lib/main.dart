d325a679086245e0c9c433fd34ccdbd2b019e62d
Ketan <ketantoo@gmail.com>
2020-04-06 16:40:11 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:covid19_flutter_app_2/screens/home.dart';
+import 'package:covid19_flutter_app_2/screens/info.dart';
+import 'package:covid19_flutter_app_2/screens/news.dart';
+import 'package:covid19_flutter_app_2/screens/video.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        fontFamily: "Poppins",
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  List<String> appbarTitle = [
+    "Home",
+    "News",
+    "Video",
+    "Information",
+  ];
+  List<Widget> screens = [
+    Home(),
+    News(),
+    Video(),
+    Info(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(appbarTitle[_currentIndex]),
+        leading: Icon(_currentIndex == 0
+            ? Icons.home
+            : _currentIndex == 1
+                ? Icons.book
+                : _currentIndex == 2 ? Icons.videocam : Icons.info),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10.0,
+          vertical: 10.0,
+        ),
+        child: screens[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.home,
+            ),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.book,
+            ),
+            title: Text('News'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.videocam,
+            ),
+            title: Text('Videos'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.info,
+            ),
+            title: Text('Info'),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _nums = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+  ];
+  String _searchTerm = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Highlights",
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 1.5,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            width: double.infinity,
+            height: 150.0,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _nums.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey[200],
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 2.0,
+                        )
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(_nums[index]),
+                    ),
+                  );
+                }),
+          ),
+          Divider(),
+          Text(
+            "All Countries",
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 1.5,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(
+                  20.0,
+                ),
+                borderSide: BorderSide(
+                  width: 0.1,
+                ),
+              ),
+              hintText: "Search for country",
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (String value) {
+              setState(() {
+                _searchTerm = value;
+              });
+            },
+          ),
+          Center(
+            child: Text(_searchTerm),
+          ),
+          Expanded(
+            child: Container(
+              height: 400,
+              child: ListView.builder(
+                itemCount: _nums.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                      margin: EdgeInsets.all(10.0),
+                      width: double.infinity,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white,
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey[200],
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 2.0,
+                          )
+                        ],
+                      ));
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
